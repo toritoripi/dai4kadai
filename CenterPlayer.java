53d8b7b912d4b02dbbc005f696c1fc227b5f036d
さいとう <114993632+toritoripi@users.noreply.github.com>
2022-10-16 10:22:04 +0900

    第4課題　修正①
    ・.forEach(out::println);　→　.forEach(System.out::println); 
    ・2行目の削除
    System.outであることが一目でわかるようにするための変更。

--- CenterPlayer.java
+++ CenterPlayer.java
@@ -1,10 +1,9 @@
 import java.util.List;
-import static java.lang.System.out;
 
 public class CenterPlayer {
 
     public static void main(String[] args) {
         List<String> playerList = List.of("桜木", "魚住", "赤木", "河田", "花形");
-        playerList.stream().filter(player -> player.contains("木")).forEach(out::println);
+        playerList.stream().filter(player -> player.contains("木")).forEach(System.out::println);
     }
 }
